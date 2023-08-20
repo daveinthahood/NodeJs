582f66f3f5a9f723df50083cbb8032320fc09ead
daveinthahood <davidesimone96@outlook.it>
2023-08-20 15:25:55 +0200

    d

---
+++ Esercizio10/server.js
@@ -0,0 +1,35 @@
+// prima cosa import il dotenv
+
+require("dotenv").config()
+//importo express
+const express = require("express")
+const app = express()
+// importo il database sistemandolo per un file js e non typescript
+
+const planet = [{
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },]
+
+// uso app use 
+
+app.use(express.urlencoded());
+app.use(express.json())
+
+// scrivo il path per vedere se il server funziona 
+app.get("/", (_, res) => {
+    return res.status(200).json({message:"online"})
+})
+
+//avviamo il server
+
+const online = process.env.SERVER_PORT;
+app.listen(online,() => {
+    console.log(`server online ${online}`);
+})
+
+// il server funziona!
