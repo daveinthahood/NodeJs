7133b9b3418f2d1cc4d91044f57b092044c002b0
daveinthahood <davidesimone96@outlook.it>
2023-09-05 17:40:42 +0200

    sd

--- Esercizio12/dist/controllers/planet.js
+++ Esercizio12/dist/controllers/planet.js
@@ -1,33 +1,55 @@
 // spostiamo il database
-let planets = [
-    { id: 1, name: "Earth" },
-    { id: 2, name: "Mars" },
+
+import { Request, Response } from "express";
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets:Planets = [
+  { id: 1, name: "Earth" },
+  { id: 2, name: "Mars" },
 ];
-const getAll = (req, res) => {
-    res.status(200).json(planets);
+
+const getAll = (req:Request, res:Response) => {
+  res.status(200).json(planets);
 };
-const getOneById = (req, res) => {
-    const { id } = req.params;
-    const planet = planets.find((p) => p.id === Number(id));
-    res.status(200).json(planet); // stampiamo un determinato id (stampa correttamente)
+
+const getOneById = (req:Request, res:Response) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet); // stampiamo un determinato id (stampa correttamente)
 };
-const create = (req, res) => {
-    const { id, name } = req.body; // diciamo dove mandare
-    const newPlanet = { id, name }; // diciamo da cosa è composto il nuovo planet
-    planets = [...planets, newPlanet]; // prendiamo il vecchio planet ed aggiungiamo il nuovo
-    res.status(201).json({ msg: "planet add" }); // su postman stampa questo messaggio e nel terminale stampa il codice 201
-    console.log(planets); // metto questo console per visualizzare nel terminale
+
+const create = (req:Request, res:Response) => {
+  const { id, name } = req.body; // diciamo dove mandare
+  const newPlanet = { id, name }; // diciamo da cosa è composto il nuovo planet
+
+  planets = [...planets, newPlanet]; // prendiamo il vecchio planet ed aggiungiamo il nuovo
+
+  res.status(201).json({ msg: "planet add" }); // su postman stampa questo messaggio e nel terminale stampa il codice 201
+
+  console.log(planets); // metto questo console per visualizzare nel terminale
 };
-const updateById = (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    planets = planets.map((p) => (p.id === Number(id) ? Object.assign(Object.assign({}, p), { name }) : p));
-    console.log(planets);
-    res.status(200).json({ msg: "update" });
+
+const updateById = (req:Request, res:Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  console.log(planets);
+  res.status(200).json({ msg: "update" });
 };
-const deleteById = (req, res) => {
-    const { id } = req.params;
-    planets = planets.filter((p) => p.id !== Number(id));
-    res.status(200).json({ msg: "planet delete" });
+
+const deleteById = (req:Request, res:Response) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "planet delete" });
 };
-export { getAll, getOneById, create, updateById, deleteById };
+
+export {
+    getAll, getOneById, create, updateById, deleteById
+}
