add586c61dd3ac05bc5edb5b0e4b83b21d45b72a
daveinthahood <davidesimone96@outlook.it>
2023-09-10 19:10:42 +0200

    d

---
+++ Esercizio15/passport.ts
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+import passport from "passport"
+import passportJWT from "passport-jwt"
+import { db } from "./db"
+const {SECRET_KEY} = process.env
+
+
+passport.use(
+    new passportJWT.Strategy({
+     secretOrKey: SECRET_KEY,
+     jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(), 
+    }, 
+    async(payload, done) => {
+        const user = await db.one(`SELECT * FROM users WHERE id= $1`, payload.id)
+        console.log(user);
+        
+        try {
+            return user ? done(null, user): done(new Error("user not found"))
+        } catch (error) {
+            done(error)
+        }
+    })
+)
