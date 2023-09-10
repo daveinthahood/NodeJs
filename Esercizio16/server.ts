fe1d482b48f0c889fd440af35dae4433b9bab863
daveinthahood <davidesimone96@outlook.it>
2023-09-10 19:10:55 +0200

    d

---
+++ Esercizio16/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan"
+import {
+    getAll, getOneById, create, updateById, deleteById, createImage
+} from "./planet"
+import multer from "multer"
+import {login, signup} from "./login"
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploaded")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload =  multer({storage})
+
+
+const app = express()
+const port = 3000
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.get("/api/planets", getAll)
+app.get("/api/planets/:id", getOneById)
+app.post("/api/planets", create)
+app.put("/api/planets/:id", updateById)
+app.delete("/api/planets/:id", deleteById)
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage)
+
+app.post("/api/users/login", login)
+app.post ("/api/users/signup", signup)
+
+
+app.listen(port, () => {
+    console.log(`App listen on port ${port}`);
+    
+})
